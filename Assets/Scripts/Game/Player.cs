6a8e91b910c7ac781a5ff646761f490263c8ce4c
Hân <hanphan1509@gmail.com>
2024-02-11 16:41:32 +0700

    move 4 directions

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -41,6 +41,24 @@ public class Player : MonoBehaviour
                 target = this.transform.position + posMoving;
                 Moving(current, target);
                 break;
+            case StatePlayer.MoveBackward:
+                posMoving = Vector3.back;
+                current = this.transform.position;
+                target = this.transform.position + posMoving;
+                Moving(current, target);
+                break;
+            case StatePlayer.MoveLeft:
+                posMoving = Vector3.left;
+                current = this.transform.position;
+                target = this.transform.position + posMoving;
+                Moving(current, target);
+                break;
+            case StatePlayer.MoveRight:
+                posMoving = Vector3.right;
+                current = this.transform.position;
+                target = this.transform.position + posMoving;
+                Moving(current, target);
+                break;
         }
     }
 
@@ -53,16 +71,15 @@ public class Player : MonoBehaviour
     public void CheckBrick()
     {
         RaycastHit hit;
-        bool isBrick = Physics.Raycast(this.transform.position + Vector3.forward, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity);
-        Debug.DrawRay(this.transform.position + Vector3.forward, transform.TransformDirection(Vector3.down), Color.black, Mathf.Infinity);
-        //Debug.Log($"HAN 1 {hit.collider.name}");
+        Physics.Raycast(this.transform.position + posMoving, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity);
+        Debug.DrawRay(this.transform.position + posMoving, transform.TransformDirection(Vector3.down), Color.black, Mathf.Infinity);
         if (hit.collider != null)
         {
-            if (!hit.collider.name.Contains("Brick"))
-            {
-                Debug.Log("Hann collider " + hit.collider.name);
-                statePlayer = StatePlayer.Idle;
-            }
+            //if (!hit.collider.name.Contains("Brick"))
+            //{
+            //    Debug.Log("Hann collider " + hit.collider.name);
+            //    statePlayer = StatePlayer.Idle;
+            //}
         }
         else
         {
